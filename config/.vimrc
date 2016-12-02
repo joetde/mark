1e196f7fac7da7f2291991af685acf113be28eff
Guillaume Berard <berardgui@gmail.com>
2016-12-02 15:27:26 -0800

    Vimrc minimal setup

---
+++ config/.vimrc
@@ -0,0 +1,21 @@
+" Found in https://dougblack.io/words/a-good-vimrc.html
+
+syntax enable
+
+set tabstop=4
+set softtabstop=4
+set expandtab " convert tabs to spaces
+au FileType make set noexpandtab et " tabs are real tabs for Makefiles
+
+" set number
+set showcmd
+set cursorline
+
+set wildmenu
+set lazyredraw
+
+set showmatch
+
+set incsearch
+set hlsearch
+
--- tools/mark-set.sh
+++ tools/mark-set.sh
@@ -6,13 +6,18 @@ export MARK_REPO=$MARK_PATH/repo
 mkdir $MARK_DEPS
 
 ### Get repo and deps ###
-echo "==Day:01== Okay folks, time to gather all that we need here..."
+echo "==Sol:01== Okay folks, time to gather all that we need here..."
 wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -O $MARK_DEPS/git-prompt.sh
 
 ### Bash config ###
-echo "==Day:02== Mark speaking, setting up our beloved bash config"
+echo "==Sol:02== Mark speaking, setting up our beloved bash config"
 echo ". $MARK_REPO/config/.bashrc" > ~/.bashrc
 
 ### Git config ###
-echo "==Day:03== Here's Mark, ready to set git configuration"
+echo "==Sol:03== Here's Mark, ready to set git configuration"
 printf "[include]\n\tpath = $MARK_REPO/config/.gitconfig" > ~/.gitconfig
+
+### Vim config ##
+echo "==Sol:04== Hey! Now setting up vim config"
+printf "source $MARK_REPO/config/.vimrc" > ~/.vimrc
+
